e2da7d9ea606340b374a97ad13757996ebb987aa
Shrish Tripathi <shrishtripof@gmail.com>
2024-03-16 00:14:47 +0530

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from '../../components/card/Card'
+import './home.css'
+const home = () => {
+  return (
+    <div className='card-array'>
+      {
+        [...Array(14)].map((e, i) => <Card key={i} />)
+      }
+    </div>
+  )
+}
+
+export default home
